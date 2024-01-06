0ad4f684bb4ca2c0a14e7d4ce15d82f4b99bde1f
Felix Swinkels <felix@code.nl>
2024-01-07 00:42:41 +0100

    Refactor function with ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,21 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
+export function activate(context: vscode.ExtensionContext): void {
+  let disposable: vscode.Disposable = vscode.commands.registerCommand(
     "extension.restartLastCommand",
-    () => {
-      const terminal = vscode.window.activeTerminal;
-      if (terminal) {
-        terminal.sendText("\u0003"); // Send Ctrl+C
-        terminal.sendText("!!"); // Send command to rerun last command
-      } else {
-        vscode.window.showInformationMessage("No active terminal");
-      }
+    (): void => {
+      const terminal: vscode.Terminal | undefined =
+        vscode.window.activeTerminal; // Get active terminal
+
+      terminal?.sendText("\u0003"); // Send Ctrl+C
+      terminal?.sendText("!!"); // Send command to rerun last command
+      terminal?.show(); // Show terminal
+
+      !terminal && vscode.window.showInformationMessage("No active terminal"); // Show error message if no active terminal
     }
   );
 
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate(): void {}
